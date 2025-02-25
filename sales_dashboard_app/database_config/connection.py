8ebe503b78e895a77ca9be5504177d668669647f
laura rodriguez <laura.rodriguez.king@outlook.com>
2025-02-25 15:38:58 -0500

    fix parent path

--- sales_dashboard_app/database_config/connection.py
+++ sales_dashboard_app/database_config/connection.py
@@ -3,15 +3,12 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from pathlib import Path
 
-# RESOLVER!!!!
-# DATA_DIR = Path("sales_dashboard_app/data")
-# DB_FILE = DATA_DIR / "sales_dashboard_data.db"
-DB_FILE = (
-    Path(__file__).resolve().parent
-    / "C:/Users/Laura Rodriguez King/Documents/Development/DataEngTest/sales_dashboard_app/data/sales_dashboard_data.db"
-)
+# Location database file
+BASE_DIR = Path(__file__).resolve().parent.parent
+DB_FILE = BASE_DIR / "data" / "sales_dashboard_data.db"
 SQLALCHEMY_DATABASE_URL = f"sqlite:///{DB_FILE}"
 
+# Connection engine
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
