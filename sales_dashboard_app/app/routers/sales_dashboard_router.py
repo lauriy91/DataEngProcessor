325c6eebf9c21fd34faadd46c85b85c5e56d5579
laura rodriguez <laura.rodriguez.king@outlook.com>
2025-02-25 13:11:51 -0500

    endpoint GET /sales/day working and testing

--- sales_dashboard_app/app/routers/sales_dashboard_router.py
+++ sales_dashboard_app/app/routers/sales_dashboard_router.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Query, Depends
 from sqlalchemy.orm import Session
+from datetime import date
 from database_config.connection import get_db
 from app.services.sales_dashboard_service import SalesDashboardService
 
@@ -14,3 +15,12 @@ def get_sales_by_product(
     db: Session = Depends(get_db),
 ):
     return SalesDashboardService.get_sales_by_product_service(db, product, category)
+
+
+@router.get("/day")
+def get_total_sales_by_day(
+    start_date: date = Query(None, description="Fecha de inicio (YYYY-MM-DD)"),
+    end_date: date = Query(None, description="Fecha de fin (YYYY-MM-DD)"),
+    db: Session = Depends(get_db),
+):
+    return SalesDashboardService.get_total_sales_by_day(db, start_date, end_date)
--- sales_dashboard_app/app/services/sales_dashboard_service.py
+++ sales_dashboard_app/app/services/sales_dashboard_service.py
@@ -1,15 +1,19 @@
 from sqlalchemy.orm import Session
 from sqlalchemy import func
+from datetime import date
 from database_config.models import Transactions
+
 
 class SalesDashboardService:
     @staticmethod
-    def get_sales_by_product_service(db: Session, product: str = None, category: str = None):
+    def get_sales_by_product_service(
+        db: Session, product: str = None, category: str = None
+    ):
         query = db.query(
             Transactions.product,
             func.sum(Transactions.total_sales).label("total_sales"),
             func.sum(Transactions.total_revenue).label("total_revenue"),
-            func.avg(Transactions.average_price).label("average_price")
+            func.avg(Transactions.average_price).label("average_price"),
         ).group_by(Transactions.product)
 
         if product:
@@ -18,14 +22,42 @@ class SalesDashboardService:
             query = query.filter(Transactions.category == category)
 
         results = query.all()
-    
+
         # Convert results to list of dictionaries
         return [
             {
                 "product": row[0],
                 "total_sales": row[1],
                 "total_revenue": row[2],
-                "average_price": row[3]
+                "average_price": row[3],
+            }
+            for row in results
+        ]
+
+    @staticmethod
+    def get_total_sales_by_day(
+        db: Session, start_date: date = None, end_date: date = None
+    ):
+        query = db.query(
+            Transactions.date,
+            func.sum(Transactions.total_sales).label("total_sales"),
+            func.sum(Transactions.total_revenue).label("total_revenue"),
+        ).group_by(Transactions.date)
+
+        if start_date:
+            query = query.filter(Transactions.date >= start_date)
+        if end_date:
+            query = query.filter(Transactions.date <= end_date)
+
+        # return query.all()
+        results = query.all()
+
+        # Convert results to list of dictionaries
+        return [
+            {
+                "date": row[0].strftime("%Y-%m-%d"),
+                "total_sales": row[1],
+                "total_revenue": row[2],
             }
             for row in results
         ]
